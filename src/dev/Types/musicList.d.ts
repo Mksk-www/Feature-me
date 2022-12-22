02df9e6adb4803852584b28d44f812ceb90c6237
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-23 02:17:58 +0900

    added types

---
+++ src/dev/Types/musicList.d.ts
@@ -0,0 +1,10 @@
+interface musicListContent {
+    name: string
+    bpm: number
+    lev: number|string
+    diff: string
+    nd: string
+    duration: string
+    dir: string
+    artist:string
+}
