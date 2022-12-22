8814b05db3683f6375c11f15e30f633c8fe44c6c
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-23 02:17:49 +0900

    updated router

--- src/dev/Routes/router.tsx
+++ src/dev/Routes/router.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Title from "Pages/Title/title";
+import Home from "Pages/home/home";
 
 const PageRouter: React.FC = () => {
     return (
         <Routes>
-            <Route path="/" element={<Title />} />
+            <Route path="/" element={<Home />} />
         </Routes>
     )
 }
