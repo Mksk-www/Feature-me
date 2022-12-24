c780101b7a14a1e577baff4a659756564fe2767c
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-24 20:43:26 +0900

    updated title

--- src/dev/Pages/musicGame/musicTitle.tsx
+++ src/dev/Pages/musicGame/musicTitle.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useAtomValue, useSetAtom } from "jotai";
 import path from "path-browserify";
+import easings from "Utils/easing/easing";
 
-import headerState from "State/headerState";
 import selectedMusicState from "State/selectedMusicState";
+import gameDataState from "State/gameState";
 
 import musicList from "Config/musicList.json"
 
@@ -13,10 +14,14 @@ import style from "./musicGame.scss";
 import thumbnail from "Assets/Images/music.png";
 import MusicLoadError from "Utils/Error/musicLoadError";
 import { motion, useAnimation } from "framer-motion";
-import easings from "Utils/easing/easing";
+import sleep from "Utils/sleep/sleep";
+
+
 
 const MusicTitle: React.FC = () => {
+    const gameData = useAtomValue(gameDataState);
     const selectedMusic = useAtomValue(selectedMusicState);
+    const titleRef = React.useRef<HTMLDivElement>(null);
     const currentMusic = musicList.musicList.find(m => m.name == selectedMusic)
 
     const animationController = useAnimation()
@@ -31,11 +36,26 @@ const MusicTitle: React.FC = () => {
     const thumbnailImage = path.join("musics", currentMusic?.dir, "thumbnail.png");
 
     React.useEffect(() => {
+        if (titleRef.current) titleRef.current.style.visibility = "";
         animationController.start(fadeIn);
     }, [])
 
+    React.useEffect(() => {
+        let timeout:NodeJS.Timeout
+        if (gameData.ready) {
+            timeout = setTimeout(async()=>{
+                animationController.start(fadeOut);
+                await sleep(200);
+                if (titleRef.current) titleRef.current.style.visibility="none";
+            },3000)
+        }
+        return ()=>{
+            clearTimeout(timeout)
+        }
+    }, [gameData.ready])
+
     return (
-        <motion.div className={style.musicTitle} animate={animationController}>
+        <motion.div className={style.musicTitle} animate={animationController} ref={titleRef}>
             <motion.div className={style.inner} animate={innerAnimation} initial={innerAnimationInitial}>
                 <img draggable="false" src={thumbnailImage} onError={(e) => e.currentTarget.src = thumbnail} />
                 <h1>{currentMusic.name}</h1>
