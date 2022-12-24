012b2cb3d8abbeb8953dd5d8403c22d4718a9eab
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-24 16:55:51 +0900

    added gameplay page

---
+++ src/dev/Pages/settings/contents/Gameplay.tsx
@@ -0,0 +1,52 @@
+import HorizonalSelectFromArray from "Components/horizonalSelectFromArray/horizonalSelectFromArray";
+import NumberInput from "Components/numberInput/numberInput";
+import { useSetAtom } from "jotai";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import headerState from "State/headerState";
+
+import style from "./contents.scss";
+import KeybindSettings from "./keybindSettings/keybindSettings";
+
+const GameplaySettings: React.FC = () => {
+
+    const [gameplaySettings, setGameplaySettings] = React.useState<gameplaySettings>(JSON.parse(localStorage.getItem("gameplaySettings") || "{}"))
+    const navigate = useNavigate();
+    const setTitle = useSetAtom(headerState);
+
+    const selector = [
+        { label: "Enabled", value: true },
+        { label: "Disabled", value: false },
+    ]
+
+    React.useEffect(() => {
+        //update title
+        setTitle("Settings - Gameplay")
+
+    }, [])
+
+    React.useEffect(() => {
+        localStorage.setItem("gameplaySettings", JSON.stringify(gameplaySettings))
+    }, [gameplaySettings])
+
+    return (
+        <div className={style.page}>
+            <h1>Gameplay</h1>
+            <div className={style.setting}>
+                <h2>Keybinds</h2>
+                <KeybindSettings />
+            </div>
+            <div className={style.setting}>
+                <h2>Show judge VFX</h2>
+                <HorizonalSelectFromArray contents={selector} value={selector.find(s => s.value == gameplaySettings.VFX) || selector[0]} onChange={(value:selectContents<boolean>) => setGameplaySettings((s: gameplaySettings) => { return { ...s, VFX: value.value } })} />
+            </div>
+            <div className={style.setting}>
+                <h2>Show FAST/LATE</h2>
+                <HorizonalSelectFromArray contents={selector} value={selector.find(s => s.value == gameplaySettings.fastLate) || selector[0]} onChange={(value: selectContents<boolean>) => setGameplaySettings((s: gameplaySettings) => { return { ...s, fastLate: value.value } })} />
+            </div>
+            {/* add judge timing settings */}
+        </div>
+    )
+}
+
+export default GameplaySettings;
