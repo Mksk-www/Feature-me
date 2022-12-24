08dd99500c58c1df0494d21a37764d3a6e3b74f4
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-24 16:56:35 +0900

    updated local storage

--- src/dev/Utils/Storage/LocalStorage/initLocalStorage.tsx
+++ src/dev/Utils/Storage/LocalStorage/initLocalStorage.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { enviroment, resourcesDownloaded, DBVersion, gameConfig, musicSelect } from "./defaultValue";
+import { audioSettings, gameplaySettings, graphicsSettings } from "./defaultValue";
 
 function initLocalStorage(): void {
     try {
         
-        localStorage.setItem("environment", JSON.stringify({ ...enviroment, ...(JSON.parse(localStorage.getItem("environment")||"{}")) }));
-        localStorage.setItem("resourcesDownloaded", JSON.stringify({ ...resourcesDownloaded, ...(JSON.parse(localStorage.getItem("resourcesDownloaded") || "{}")) }));
-        localStorage.setItem("DBVersion", JSON.stringify({ ...DBVersion, ...(JSON.parse(localStorage.getItem("DBVersion") || "{}")) }));
-        localStorage.setItem("gameConfig", JSON.stringify({ ...gameConfig, ...(JSON.parse(localStorage.getItem("gameConfig") || "{}")) }));
-        localStorage.setItem("musicSelect", JSON.stringify({ ...musicSelect, ...(JSON.parse(localStorage.getItem("musicSelect") || "{}")) }));
+        localStorage.setItem("graphicsSettings", JSON.stringify({...graphicsSettings, ...JSON.parse(localStorage.getItem("graphicsSettings")||"{}")}));
+        localStorage.setItem("audioSettings", JSON.stringify({ ...audioSettings, ...JSON.parse(localStorage.getItem("audioSettings") || "{}") }));
+        localStorage.setItem("gameplaySettings", JSON.stringify({ ...gameplaySettings, ...JSON.parse(localStorage.getItem("gameplaySettings") || "{}") }));
+        //localStorage.setItem("musicSelect", JSON.stringify({ ...musicSelect, ...(JSON.parse(localStorage.getItem("musicSelect") || "{}")) }));
     
 } catch (error) {
     console.error(error);
