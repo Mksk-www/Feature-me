d2767f87c36cd3bc277b2ca9dee56428b93d797d
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-24 16:56:26 +0900

    added new types

---
+++ src/dev/Types/config/localstorage.d.ts
@@ -0,0 +1,17 @@
+interface graphicsSettings {
+    fps:number
+    resolution:number
+}
+
+interface audioSettings {
+    master:number
+    music:number
+    effect:number
+}
+
+interface gameplaySettings {
+    keybinds:Array<string>
+    VFX:boolean
+    fastLate:boolean
+    vLine:boolean
+}
