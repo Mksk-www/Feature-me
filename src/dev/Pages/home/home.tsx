2a05ecd1ab0320df7cfef8030bee639e741e091f
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-24 16:55:11 +0900

    updated home page

--- src/dev/Pages/home/home.tsx
+++ src/dev/Pages/home/home.tsx
@@ -4,8 +4,29 @@ import musicList from "Config/musicList.json";
 
 import style from "./home.scss";
 import MusicListCard from "./musicListCard";
+import { useNavigate } from "react-router-dom";
+import { useSetAtom } from "jotai";
+import headerState from "State/headerState";
 
 const Home:React.FC = () => {
+    const navigate = useNavigate();
+    const setTitle = useSetAtom(headerState);
+
+    React.useEffect(()=>{
+        //update title
+        setTitle("Home");
+        //add shortcut
+        window.addEventListener("keydown",(e)=>{
+            if(e.code=="Escape") navigate("/settings");
+        })
+
+        return()=>{
+            window.removeEventListener("keydown", (e) => {
+                if (e.code == "Escape") navigate("/settings");
+            })
+        }
+    },[])
+
     return(
         <div className={style.home}>
             {
