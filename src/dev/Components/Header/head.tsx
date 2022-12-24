5e6be807ceb3ceef2432b2e416d91b7b06cf1528
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-24 16:54:40 +0900

    updated Header

--- src/dev/Components/Header/head.tsx
+++ src/dev/Components/Header/head.tsx
@@ -1,16 +1,31 @@
 import React from "react";
+import {BsWrench} from "react-icons/bs";
 
 import style from "./head.scss";
 import { useAtomValue } from "jotai";
 import headerState from "State/headerState";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
 
     const title = useAtomValue(headerState);
 
+    function navigateSettings(){
+        if(location.pathname.includes("/settings")) navigate("/");
+        else navigate("/settings");
+    }
+
     return (
         <header className={style.header}>
-                <h1>{title||"Home"}</h1>
+            <h1>{title||"Home"}</h1>
+
+            <div className={style.icons}>
+                <div className={style.icon} onClick={navigateSettings}>
+                    <BsWrench />
+                </div>
+            </div>
         </header>
     )
 }
