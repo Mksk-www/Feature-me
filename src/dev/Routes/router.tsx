170940f5b39d54812130f20210ef065c9c4494e3
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-24 16:56:11 +0900

    updated router

--- src/dev/Routes/router.tsx
+++ src/dev/Routes/router.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Home from "Pages/home/home";
 
+import SettingsPage from "Pages/settings/settings";
+
 const PageRouter: React.FC = () => {
+    const location = useLocation()
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/settings/*" element={<SettingsPage />} />
         </Routes>
     )
 }
