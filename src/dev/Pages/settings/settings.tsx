1f793f3acd227b0001eea957d724862bf1f0faaf
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-24 16:55:20 +0900

    added settings

---
+++ src/dev/Pages/settings/settings.tsx
@@ -0,0 +1,55 @@
+import LinkWrapper from "Components/linkWrapper/linkWrapper";
+import { useSetAtom } from "jotai";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import SettingsRouter from "Routes/settingsRouter/settingsRouter";
+import headerState from "State/headerState";
+
+import style from "./settings.scss";
+
+const SettingsPage: React.FC = () => {
+
+    const navigate = useNavigate();
+    const setTitle = useSetAtom(headerState);
+    const [currentTab,setCurrentTab] = React.useState("./");
+
+    const settingsTab = [
+        { label: "General", to: "./" },
+        { label: "Graphics", to: "./graphics" },
+        { label: "Audio", to: "./audio" },
+        { label: "Gameplay", to: "./gameplay" },
+
+    ]
+
+    React.useEffect(() => {
+        //update title
+        setTitle("Settings")
+        window.addEventListener("keydown", (e) => {
+            if (e.code == "Escape") navigate("/");
+        })
+
+        return () => {
+            window.removeEventListener("keydown", (e) => {
+                if (e.code == "Escape") navigate("/settings");
+            })
+        }
+    }, [])
+
+    return (
+        <div className={style.settingsPage}>
+            <div className={style.tabs}>
+                {
+                    settingsTab.map(t=>{
+                        const isActive = t.to == currentTab;
+                        return(
+                            <LinkWrapper to={t.to} className={`${style.tab} ${isActive&&style.active}`} onClick={()=>setCurrentTab(t.to)}>{t.label}</LinkWrapper>
+                        )
+                    })
+                }
+            </div>
+            <SettingsRouter />
+        </div>
+    )
+}
+
+export default SettingsPage;
