e899da725b23d615d92aace02a456b7f8f6d9e34
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-26 01:59:15 +0900

    added judge table

---
+++ src/dev/Features/judgeTable.ts
@@ -0,0 +1,27 @@
+const judgeTable:judgeTable = {
+    stunningBloom: {
+        key: "stunningBloom",
+        label: "Stunning Bloom",
+        range: 45,
+        scoreMultiplier: 1,
+        color:0xeabc02
+    },
+    bloom: {
+        key: "bloom",
+        label: "Bloom",
+        range: 90,
+        scoreMultiplier: 0.5,
+        color: 0x35c101
+    },
+    miss: {
+        key: "miss",
+        label: "Miss",
+        range: 135,
+        scoreMultiplier: 0,
+        color: 0xaaaaaa
+    }
+}
+
+
+
+export default judgeTable;
