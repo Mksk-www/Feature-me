afbfc2426a243ce068cfcca2eadfc452329bef5e
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-26 01:59:10 +0900

    updated classes

--- src/dev/Features/noteClasses.ts
+++ src/dev/Features/noteClasses.ts
@@ -4,36 +4,44 @@ import degToRad from "Utils/degreeToRadian/degreeToRadian";
 class note {
     judged: boolean = false;
     active: boolean = false;
-    constructor() { }
+    targetTime: number = 0;
+    visual: PIXI.Graphics = new PIXI.Graphics();
+    scrollTime?: number;
+    keyId: number;
+    constructor(track: number, BPM: number, count: number) {
+        this.targetTime = (60 / BPM * count) * 1000;
+        this.keyId = track;
+    }
 }
 
 class tapNote extends note {
-    visual = new PIXI.Graphics().beginFill(0xf5f5f5).drawRect(0, 0, 240, 8)
-    constructor(track: number, count: number) {
-        super()
-        this.visual.x = 240 * track
+    constructor(track: number, BPM: number, count: number) {
+        super(track, BPM, count);
+        this.visual = new PIXI.Graphics().beginFill(0xf5f5f5).drawRect(0, 0, 240, 8);
+        this.visual.x = 240 * track;
     }
 }
 
 class brightNote extends note {
-    visual = new PIXI.Graphics().beginFill(0xeabc02).drawRect(0, 0, 240, 12)
-    constructor(track: number, count: number) {
-        super();
+    keyId = 4;
+    constructor(BPM: number, count: number) {
+        super(4, BPM, count);
+        this.visual = new PIXI.Graphics().beginFill(0xeabc02).drawRect(0, 0, 960, 12)
         this.visual.x = 0;
-
     }
 }
 
 class seedNote extends note {
-    visual = new PIXI.Graphics().beginFill(0x02f768).drawRect(0, 0, 80, 80)
-    constructor(track: number, count: number) {
-        super();
+    LR: "left" | "right";
+    constructor(track: number, BPM: number, count: number) {
+        super(track, BPM, count);
+        this.visual = new PIXI.Graphics().beginFill(0x02f768, 0.75).drawRect(0, 0, 60, 60);
         this.visual.pivot.set(40);
         this.visual.rotation = degToRad(45);
         this.visual.x = 60;
-
+        this.LR = track == 5 ? "left" : "right";
     }
 }
 
 
-export { tapNote, brightNote, seedNote };
+export { note, tapNote, brightNote, seedNote };
