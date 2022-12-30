4276ce8de116e4b27970d5fcbaa4ac2b4ceceefb
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-30 16:37:28 +0900

    updated general

---
+++ src/dev/Pages/settings/contents/General.tsx
@@ -0,0 +1,33 @@
+import { useSetAtom } from "jotai";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import SettingsRouter from "Routes/settingsRouter/settingsRouter";
+import headerState from "State/headerState";
+
+import style from "./contents.scss";
+
+const GeneralSettings: React.FC = () => {
+
+    const navigate = useNavigate();
+    const setTitle = useSetAtom(headerState);
+
+    React.useEffect(() => {
+        //update title
+        setTitle("Settings - General")
+
+    }, [])
+
+    return (
+        <div className={style.page}>
+            <h1>General</h1>
+            <h2>Game information</h2>
+            <h3>Feature Me Alpha 0.4.0</h3>
+
+            <h2>Links</h2>
+            <a target="_blank" href="https://feature-me.github.io/Feature-me-Alpha-v0.x/">Alpha 0.3.7</a>
+            <a target="_blank" href="https://feature-me-v1.onrender.com/">Feature Me</a>
+        </div>
+    )
+}
+
+export default GeneralSettings;
