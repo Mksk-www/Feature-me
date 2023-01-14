60b478f02e69ad99c4e5220b1d16b80f9a61d3de
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-14 15:35:11 +0900

    added setting footer key hint

--- src/dev/Pages/home/home.tsx
+++ src/dev/Pages/home/home.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useSetAtom } from "jotai";
+import { BiPointer } from "react-icons/bi";
 
 import MusicListCard from "./musicListCard";
 import headerState from "State/headerState";
 
 import musicList from "Config/musicList.json";
 
-import style from "./home.scss";
 import selectedMusicState from "State/selectedMusicState";
 import gameDataState, { initialChart } from "State/gameState";
+import footerState from "State/footerState";
 
-const Home:React.FC = () => {
+import style from "./home.scss";
+
+const Home: React.FC = () => {
     const navigate = useNavigate();
     const setTitle = useSetAtom(headerState);
     const setSelectedMusic = useSetAtom(selectedMusicState);
     const setGameData = useSetAtom(gameDataState);
-    
+    const setFooter = useSetAtom(footerState);
+
     //delete game cache
     function initializeCache() {
         setGameData({
@@ -26,28 +30,30 @@ const Home:React.FC = () => {
         })
     }
 
-    React.useEffect(()=>{
+    React.useEffect(() => {
         //update title
         initializeCache();
         setTitle("Home");
+        //set footer
+        setFooter([{ icon: <BiPointer />, value: "Select" }])
         //add shortcut
         setSelectedMusic("");
-        window.addEventListener("keydown",(e)=>{
-            if(e.code=="Escape") navigate("/settings");
+        window.addEventListener("keydown", (e) => {
+            if (e.code == "Escape") navigate("/settings");
         })
 
-        return()=>{
+        return () => {
             window.removeEventListener("keydown", (e) => {
                 if (e.code == "Escape") navigate("/settings");
             })
         }
-    },[])
+    }, [])
 
-    return(
+    return (
         <div className={style.home}>
             {
-                musicList.musicList.map((c)=>{
-                    return(
+                musicList.musicList.map((c) => {
+                    return (
                         <MusicListCard content={c} key={c.name} />
                     )
                 })
