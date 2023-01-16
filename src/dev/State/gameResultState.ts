b37a94e77206b6edd7b300c06aa3063b9258c851
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-16 21:59:55 +0900

    updated state

--- src/dev/State/gameResultState.ts
+++ src/dev/State/gameResultState.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 
-const initialResult = {
+const initialResult:gameResultState = {
     score: 0,
     judges: {
         stunningBloom: 0,
@@ -11,8 +11,7 @@ const initialResult = {
         fast: 0,
         late: 0
     },
-    chain: 0,
-    maxChain: 0
+    maxChain: 0,
 }
 
 const gameResultState = atom<gameResultState>(initialResult)
