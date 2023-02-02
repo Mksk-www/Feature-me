5a4c89a076cf6328d70b78956ae0ac8a1b09777d
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-02 21:33:33 +0900

    updated router

--- src/dev/Routes/router.tsx
+++ src/dev/Routes/router.tsx
@@ -6,15 +6,16 @@ import Home from "Pages/home/home";
 import SettingsPage from "Pages/settings/settings";
 import MusicGame from "Pages/musicGame/musicGame";
 import ResultPage from "Pages/result/result";
+import ReplayRouter from "./replayRouter";
 
 const PageRouter: React.FC = () => {
-    const location = useLocation()
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/settings/*" element={<SettingsPage />} />
             <Route path="/play" element={<MusicGame />} />
             <Route path="/result" element={<ResultPage />} />
+            <Route path="/replay/*" element={<ReplayRouter />} />
         </Routes>
     )
 }
