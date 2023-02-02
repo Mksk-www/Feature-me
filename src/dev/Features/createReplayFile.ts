2c003d8d6347e83f7e31dd7941fadf51d1f12d76
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-02 21:33:27 +0900

    updated create replay func

--- src/dev/Features/createReplayFile.ts
+++ src/dev/Features/createReplayFile.ts
@@ -1,10 +1,28 @@
 import arrayBufferToBase64 from "Utils/ArrayBuffertoBase64/ArrayBuffertoBase64";
 
-function createReplay(replay: replayData) {
-    const replayObject = {
+function createReplay(result: gameResultState) {
+    if (!result.replay) return;
+
+
+    const { replay } = result;
+
+    const replayObject: replayFile = {
         audio: `data:audio/mp3;base64,${arrayBufferToBase64(replay.audio)}`,
         chart: replay.chart,
-        input: replay.input
+        input: replay.input,
+        result: {
+            score: result.score,
+            maxChain: result.maxChain,
+            judges: {
+                stunningBloom: result.judges.stunningBloom,
+                bloom: result.judges.bloom,
+                miss: result.judges.miss
+            },
+            timing: {
+                fast: result.timing.fast,
+                late: result.timing.late,
+            }
+        }
     }
 
     const BOM = new Uint8Array([0xEF, 0xBB, 0xBF]);
