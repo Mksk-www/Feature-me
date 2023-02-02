710cf334326daf01c692f2ecc61ccc1cb0552d15
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-02 21:32:16 +0900

    updated header icons

--- src/dev/Components/Header/head.tsx
+++ src/dev/Components/Header/head.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BsWrench } from "react-icons/bs";
+import { BsWrench, BsCameraVideo } from "react-icons/bs";
 
 import style from "./head.scss";
 import { useAtomValue } from "jotai";
@@ -12,22 +12,25 @@ const Header: React.FC = () => {
 
     const title = useAtomValue(headerState);
 
-    function navigateSettings() {
-        if (location.pathname.includes("/settings")) navigate("/");
-        else navigate("/settings");
+    function navigateTo(target:string){
+        if (location.pathname.includes(target)) navigate("/");
+        else navigate(target);
     }
 
     React.useEffect(() => {
         document.title = `${title} - Feature Me Alpha`;
-    },[title])
+    }, [title])
 
     return (
         <header className={style.header}>
             <h1>{title || "Home"}</h1>
 
             <div className={style.icons}>
-                <div className={style.icon} onClick={navigateSettings}>
+                <div className={style.icon} onClick={()=>navigateTo("/settings")}>
                     <BsWrench />
+                </div>
+                <div className={style.icon} onClick={()=>navigateTo("/replay")}>
+                    <BsCameraVideo />
                 </div>
             </div>
         </header>
