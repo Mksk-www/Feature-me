679c019120c295cd13244c261efac8f869e6a3dd
= <=>
2024-11-09 18:46:12 +0900

    update intl

---
+++ src/i18n/en/index.ts
@@ -0,0 +1,7 @@
+import type { BaseTranslation } from '../i18n-types.js'
+import pages from "./pages/pages";
+const en = {
+    pages
+} satisfies BaseTranslation
+
+export default en
---
+++ src/i18n/ja/index.ts
@@ -0,0 +1,7 @@
+import type { Translation } from '../i18n-types.js'
+import pages from "./pages/pages";
+const ja = {
+	pages
+	} satisfies Translation
+
+export default ja
