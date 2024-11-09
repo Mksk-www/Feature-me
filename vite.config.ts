98b5fb5d31b41526cd71300ad0564102d08f76e4
= <=>
2024-11-09 11:53:05 +0900

    update config

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import {defineConfig} from "vite";
+import solid from "vite-plugin-solid"
+import devtools from "solid-devtools/vite"
+
+export default defineConfig({
+    plugins: [
+        solid(),
+        devtools({
+            autoname: true,
+            locator: {
+                targetIDE: "webstorm",
+                componentLocation: true,
+                jsxLocation: true
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            "Global/": "/src/global/",
+            "Pages/": "/src/pages/",
+            "Assets/": "/src/assets",
+            "@/": "/src/"
+        }
+    }
+})
