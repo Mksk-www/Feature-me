c821b0e80ca08339723933f43881516c0ee3d862
= <=>
2024-11-09 11:53:18 +0900

    update App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,15 @@
+import {MemoryRouter, Router} from "@solidjs/router";
+import PageRouter from "Global/routes/PageRouter.tsx";
+import {Match, Switch} from "solid-js";
+import {isDev} from "solid-js/web";
+
 export default () =>{
     return (
         <div>
+            <Switch>
+                <Match when={isDev}><Router>{PageRouter}</Router></Match>
+                <Match when={!isDev}><MemoryRouter>{PageRouter}</MemoryRouter></Match>
+            </Switch>
             
         </div>
     )
