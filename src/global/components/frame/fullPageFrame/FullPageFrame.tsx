52c7b65b2abdd51af03d374ff180a9e7e28f3545
= <=>
2024-11-09 11:53:29 +0900

    add global files

---
+++ src/global/components/frame/fullPageFrame/FullPageFrame.tsx
@@ -0,0 +1,12 @@
+import clsx from 'clsx';
+import {JSX} from "solid-js";
+import style from './FullPageFrame.module.scss';
+
+
+export default (props:JSX.HTMLAttributes<HTMLDivElement>) => {
+    return(
+        <div {...props} class={clsx(props.class,style.frame)}>
+            {props.children}
+        </div>
+    )
+}
